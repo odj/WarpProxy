5c44be960c6f3a6a2934874124c2efba1e2840e0
Orion Jankowski <orion10@gmail.com>
2012-05-05 16:33:13 -0700

    Added boilerplate functions for command line parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,48 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
-import Prelude hiding (catch)
+module Main (main) where
 
-import Network.Wai as W
-import Network.Wai.Handler.Warp
-import Network.HTTP.Conduit as H
-import qualified Data.Conduit as C
-import Blaze.ByteString.Builder.ByteString (fromByteString)
 import Blaze.ByteString.Builder (toByteString)
-import Control.Monad.IO.Class
+import Blaze.ByteString.Builder.ByteString (fromByteString)
+import Control.Concurrent
 import Control.Exception.Base (catch)
-import Data.Monoid
-import Data.Time
+import Control.Monad.IO.Class
 import Data.ByteString.Char8 (unpack)
 import Data.ByteString.Internal (ByteString)
+import qualified Data.Conduit as C
+import Data.Monoid
+import Data.Time
+import Network.HTTP.Conduit as H
+import Network.Wai as W
+import Network.Wai.Handler.Warp
+import Prelude hiding (catch)
+import System.Environment
+import System.Exit
 import System.Locale
 
 
 pharmash = "http://www.pharmash.com" :: ByteString
+
+
+sigHandler mv = do putMVar mv True
+
+getParam [] _   = Nothing
+getParam (_:[]) _ = Nothing
+getParam (a:args) flag 
+    | a == flag = Just $ head args
+    | otherwise = getParam args flag
+
+
+orElse m b = case m of
+    Just a  -> a
+    Nothing -> b
+
+orDie m s = case m of
+    Just a -> return a
+    Nothing -> do  
+        putStrLn s
+        exitFailure
 
 proxApp req = do
     liftIO $ logReq req
@@ -67,6 +91,8 @@ logReq req = do
                {-++ " -- " ++ (show $ W.requestHeaders req)-}
     
 main = do
+    {-args <- getArgs-}
+    {-logFile <- orDie (getParam args "-l") "Missing log output file"-}
     run 80 proxApp
 
 
