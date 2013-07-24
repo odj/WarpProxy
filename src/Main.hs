db10deac2be9884ee036b8800f3d6f322b437357
Orion Jankowski <orion10@gmail.com>
2013-07-24 14:53:12 -0700

    Update to latest packages.  Things have really changed a lot in WAI

--- src/Main.hs
+++ src/Main.hs
@@ -3,27 +3,29 @@
 
 module Main (main) where
 
-import Blaze.ByteString.Builder (toByteString)
 import Blaze.ByteString.Builder.ByteString (fromByteString)
 import Control.Concurrent
 import Control.Exception.Base (catch)
 import Control.Monad.IO.Class
 import Data.ByteString.Char8 (unpack)
+import qualified Data.ByteString as BS (concat)
 import Data.ByteString.Internal (ByteString)
 import qualified Data.Conduit as C
+import qualified Data.Conduit.List as CL
+import Data.Conduit.Lazy (lazyConsume)
 import Data.Monoid
 import Data.Time
 import Network.HTTP.Conduit as H
 import Network.Wai as W
 import Network.Wai.Handler.Warp
-import Prelude hiding (catch)
-import System.Environment
+import Prelude hiding (concat)
 import System.Exit
 import System.Locale
 import System.IO
+import Control.Applicative ( (<$>) )
 
 
-pharmash = "http://www.pharmash.com" :: ByteString
+pharmash = "http://hal" :: ByteString
 
 
 sigHandler mv = do putMVar mv True
@@ -45,57 +47,47 @@ orDie m s = case m of
         putStrLn s
         exitFailure
 
-proxApp req = do
+proxApp sourcePort req = do
     liftIO $ logReq req
     pRequestBase <- parseUrl $ unpack $ pharmash `mappend`  rawPathInfo req
                                                 `mappend`  rawQueryString req
-    let requestBodySrc = W.requestBody req
-    rBody <- requestBodySrc C.$$ requestSink
-    let pRequest = pRequestBase { H.requestBody = RequestBodyBS $ toByteString rBody
+    rBody <- liftIO $ mconcat <$> C.runResourceT (W.requestBody req C.$$ CL.consume)
+    {-liftIO $ putStrLn $ show rBody-}
+    let pRequest = pRequestBase { H.requestBody = RequestBodyBS (rBody)
                                 , H.method      = requestMethod req
                                 , H.requestHeaders = W.requestHeaders req
-                                , H.port        = 8080
+                                , H.port        = sourcePort
                                 }
     (return =<< liftIO) $ catch
         (H.withManager $ \manager -> do
-            Response status _ headers src <- http pRequest manager
-            body <- src C.$$ responseSink
-            return $ ResponseBuilder status headers body)
-        (\(StatusCodeException status headers) -> do
-           return $ ResponseBuilder status headers $ fromByteString "")
-
-
-responseSink = C.sinkState
-    (fromByteString "")
-    (\acc a -> return $ C.StateProcessing $ mappend acc $ fromByteString a )
-    (\acc -> return acc)
-
-
--- NB - same as responseSink for now
-requestSink = C.sinkState
-    (fromByteString "")
-    (\acc a -> return $ C.StateProcessing $ mappend acc $ fromByteString a )
-    (\acc -> return acc)
+            res <- http pRequest manager
+            let body = (H.responseBody res)
+            b <- body C.$$+- CL.consume
+            return $ ResponseBuilder (H.responseStatus res) (H.responseHeaders res) ( fromByteString $ BS.concat b) )
+        (\(StatusCodeException status headers jar) -> do
+           return $ ResponseBuilder status headers $ fromByteString "not ok")
 
 
 logReq req = do
     time <- getCurrentTime
     localeTime <- utcToLocalZonedTime time
+    rBody <- liftIO . C.runResourceT . lazyConsume . W.requestBody $ req
     let timeString = formatTime defaultTimeLocale "[ %D %X%Q ]\t" localeTime
     putStrLn $ timeString
                ++ (show $ requestMethod req)
                {-++ " -- " ++ (show $ httpVersion req)-}
                {-++ " -- " ++ (show $ serverName req)-}
-               ++ " -- " ++ (show $ rawPathInfo req)
+               {-++ " -- " ++ (show $ rawPathInfo req)-}
                ++ " -- " ++ (show $ remoteHost req)
                {-++ " -- " ++ (show $ W.queryString req)-}
-               {-++ " -- " ++ (show $ W.requestHeaders req)-}
+               ++ " -- " ++ (show $ W.requestHeaders req)
+               ++ " -- " ++ (show rBody)
     hFlush stdout
 
 main = do
     {-args <- getArgs-}
     {-logFile <- orDie (getParam args "-l") "Missing log output file"-}
-    run 80 proxApp
+    run 15080 (proxApp 15080)
 
 
 
